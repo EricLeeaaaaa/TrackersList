6ee745d2896268305f61dd17f1c7fb938da574d4
Eric Lee <98399372+EricLeeaaaaa@users.noreply.github.com>
2024-07-17 22:43:34 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,8 +5,9 @@ sources=(
     "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"
     "https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt"
     "https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt"
-    "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt"
+    "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_combine.txt"
     "https://raw.githubusercontent.com/Azathothas/Trackers/main/trackers_all.txt"
+    "https://newtrackon.com/api/all"
 )
 
 # 下载并合并所有源
@@ -15,10 +16,25 @@ curl -sS "${sources[@]}" | grep -E '^(http|udp|ws)' | sort -u > combined.txt
 # 验证并分类 trackers
 verify_tracker() {
     local tracker=$1
-    if curl -sS --connect-timeout 3 -o /dev/null "$tracker"; then
-        echo "$tracker"
-        [[ $tracker == https://* ]] && echo "$tracker" >> https.txt
-    fi
+    local protocol=$(echo "$tracker" | cut -d: -f1)
+
+    case $protocol in
+        http|https)
+            if curl -sS --connect-timeout 3 -o /dev/null "$tracker"; then
+                echo "$tracker"
+                [[ $tracker == https://* ]] && echo "$tracker" >> https.txt
+            fi
+            ;;
+        udp)
+            if nc -zu -w3 $(echo "$tracker" | cut -d/ -f3 | cut -d: -f1) $(echo "$tracker" | cut -d: -f3 | cut -d/ -f1) 2>/dev/null; then
+                echo "$tracker"
+            fi
+            ;;
+        ws|wss)
+            # 对于 WebSocket，我们暂时不验证，直接添加
+            echo "$tracker"
+            ;;
+    esac
 }
 
 export -f verify_tracker
