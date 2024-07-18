ecbb07743a380e7c8eb90ccc12208046b3cc4058
Eric Lee <98399372+EricLeeaaaaa@users.noreply.github.com>
2024-07-18 23:45:24 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,25 +10,39 @@ sources=(
     "https://newtrackon.com/api/all"
 )
 
-# 合并所有源
-curl -sS "${sources[@]}" | grep -E '^(http|udp|ws)' | sort -u > temp.txt
+# 下载并合并所有源
+curl -sS "${sources[@]}" | grep -E '^(http|udp|ws)' > temp.txt
 
-# 去重
+# 高级去重
 awk '
 BEGIN { FS = "[:/?]" }
 {
     proto = $1
     domain = $4
-    path = $5 ? "/" $5 : ""
+    port = $5
+    path = ""
     for (i = 6; i <= NF; i++) path = path "/" $i
     if (path == "") path = "/"
-    key = proto "://" domain path
-    if (!(key in seen)) {
-        seen[key] = 1
-        print $0
+    
+    # 标准化端口
+    if ((proto == "http" && port == "80") || (proto == "https" && port == "443")) {
+        port = ""
+    }
+    
+    # 构建键，忽略默认端口
+    key = proto "://" domain (port ? ":" port : "") path
+    
+    # 如果这个键是新的，或者比之前存储的URL更短，就更新它
+    if (!(key in seen) || length($0) < length(seen[key])) {
+        seen[key] = $0
     }
 }
-' temp.txt | sort > all.txt
+END {
+    for (key in seen) {
+        print seen[key]
+    }
+}
+' temp.txt | sort -u > all.txt
 
 # 提取 HTTPS trackers
 grep '^https://' all.txt > https.txt
